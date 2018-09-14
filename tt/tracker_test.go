ec82d5c29826d748fcca79caca8209536ec1eb13
Dennis Schön <mail@dennis-schoen.de>
2018-09-14 10:24:47 +0200

    test Days() and End()

--- tt/tracker_test.go
+++ tt/tracker_test.go
@@ -137,3 +137,120 @@ func Test_tracker_Start(t *testing.T) {
 		})
 	}
 }
+
+func Test_tracker_End(t *testing.T) {
+	start := time.Now().Add(time.Hour * -7)
+	end := time.Now()
+
+	tests := []struct {
+		name           string
+		end            time.Time
+		initialStorage persistence
+		wantStorage    persistence
+		wantErr        bool
+	}{
+		{
+			name:           "works",
+			end:            end,
+			initialStorage: &testStorage{times: []time.Time{start}},
+			wantStorage:    &testStorage{times: []time.Time{start, end}},
+			wantErr:        false,
+		},
+		{
+			name:           "not",
+			end:            end,
+			initialStorage: &testStorage{},
+			wantStorage:    &testStorage{},
+			wantErr:        true,
+		},
+		{
+			name:           "read failed",
+			end:            end,
+			initialStorage: &testStorage{readError: true},
+			wantStorage:    &testStorage{readError: true},
+			wantErr:        true,
+		},
+		{
+			name:           "write failed",
+			end:            end,
+			initialStorage: &testStorage{times: []time.Time{start}, writeError: true},
+			wantStorage:    &testStorage{times: []time.Time{start}, writeError: true},
+			wantErr:        true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tracker := newTracker(tt.initialStorage)
+			if err := tracker.End(tt.end); (err != nil) != tt.wantErr {
+				t.Errorf("tracker.Start() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(tracker.db, tt.wantStorage) {
+				t.Errorf("encode() = %s, want %s", tracker.db, tt.wantStorage)
+			}
+		})
+	}
+}
+
+func Test_tracker_Days(t *testing.T) {
+	testTime1, err := time.Parse("2006-01-02 15:04", "2018-09-01 10:00")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testTime2 := testTime1.Add(time.Hour * 2)
+	testTime3 := testTime1.Add(time.Hour * 4)
+	testTime4 := testTime1.Add(time.Hour * 24)
+
+	tests := []struct {
+		name    string
+		storage persistence
+		want    []*day
+		wantErr bool
+	}{
+		{
+			name:    "read failed",
+			storage: &testStorage{readError: true},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "works",
+			storage: &testStorage{times: []time.Time{testTime1, testTime2, testTime3, testTime4}},
+			want: []*day{
+				&day{
+					Date: testTime1,
+					Entries: []*entry{
+						&entry{
+							Start: testTime1,
+							End:   testTime2,
+						},
+						&entry{
+							Start: testTime3,
+						},
+					},
+				},
+				&day{
+					Date: testTime4,
+					Entries: []*entry{
+						&entry{
+							Start: testTime4,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tracker := newTracker(tt.storage)
+			got, err := tracker.Days()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("tracker.Days() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("tracker.Days() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
